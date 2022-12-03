6ebe3b852fb6ff043b9a976a0cff382661bfd29b
James Ravn <james@r-vn.org>
2022-12-03 16:51:22 +0000

    feat: day3 part2

--- day3/main.cpp
+++ day3/main.cpp
@@ -38,8 +38,7 @@ int main(int argc, char *argv[]) {
     set_intersection(first.begin(), first.end(), second.begin(), second.end(), back_inserter(dupes));
     if (dupes.size() == 1) {
       auto prio = priority(dupes[0]);
-      cout << "adding " << dupes[0] << " with priority " << prio << endl;
-      prioritiesSum += priority(dupes[0]);
+      prioritiesSum += prio;
     } else {
       cout << "bug: should have one dupe but had " << dupes.size() << endl;
 
@@ -50,7 +49,29 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  auto groupPrioritiesSum = 0;
+  for (auto group = 0; group < lines.size() / 3; group += 1) {
+    auto first = createCompartment(lines[group * 3]);
+    auto second = createCompartment(lines[group * 3 + 1]);
+    auto third = createCompartment(lines[group * 3 + 2]);
+
+    vector<char> dupes;
+    set_intersection(first.begin(), first.end(), second.begin(), second.end(), back_inserter(dupes));
+    vector<char> dupes2;
+    set_intersection(dupes.begin(), dupes.end(), third.begin(), third.end(), back_inserter(dupes2));
+
+    if (dupes2.size() == 1) {
+      auto prio = priority(dupes2[0]);
+      cout << "adding " << dupes2[0] << " with priority " << prio << endl;
+      groupPrioritiesSum += prio;
+    } else {
+      cout << "bug: should have one dupe but had " << dupes.size() << endl;
+      return 1;
+    }
+  }
+
   cout << "part 1: " << prioritiesSum << endl;
+  cout << "part 2: " << groupPrioritiesSum << endl;
 
   return 0;
 }
