b4fe34d6464c3e80dbc52473356c96b1feff16d6
James Ravn <james@r-vn.org>
2022-12-03 12:12:05 +0000

    feat: finish day1

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,5 +1,9 @@
 #include <QFile>
 #include <QTextStream>
+#include <queue>
+
+using namespace Qt;
+using namespace std;
 
 int main(int argc, char *argv[]) {
   QTextStream out(stdout);
@@ -10,19 +14,30 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  int curSum = 0, maxSum = 0;
+  priority_queue<int> snacks;
+  int curSum = 0;
 
   QTextStream in(&file);
   while (!in.atEnd()) {
     QString line = in.readLine().trimmed();
     if (line.isEmpty()) {
+      snacks.push(curSum);
       curSum = 0;
     } else {
       curSum += line.toInt();
-      maxSum = qMax(curSum, maxSum);
     }
   }
+  snacks.push(curSum);
 
-  out << "Max sack is " << maxSum << Qt::endl;
+  int top3 = 0;
+  top3 += snacks.top();
+  snacks.pop();
+  top3 += snacks.top();
+  snacks.pop();
+  top3 += snacks.top();
+  snacks.pop();
+
+  out << top3 << endl;
+
   return 0;
 }
